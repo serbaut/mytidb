64e7eba73e524ddf368d156f810ef3a71de5cfb1
Joakim Sernbrant <serbaut@gmail.com>
2021-07-16 11:32:57 +0000

    Refactor once

--- mytidb.go
+++ mytidb.go
@@ -29,63 +29,63 @@ func init() {
 	sql.Register("mytidb", &mytidb{})
 }
 
+func startTidb(dsn string) error {
+	myCfg, err := mysql.ParseDSN(dsn)
+	if err != nil {
+		return err
+	}
+	cfg := config.NewConfig()
+	cfg.Path = "/tmp/mytidb"
+	cfg.Status.ReportStatus = false
+	cfg.Log.Level = "fatal"
+	if host, sport, err := net.SplitHostPort(myCfg.Addr); err != nil {
+		return err
+	} else if iport, err := strconv.Atoi(sport); err != nil {
+		return err
+	} else {
+		cfg.Host = host
+		cfg.Port = uint(iport)
+	}
+	if err := cfg.Valid(); err != nil {
+		return err
+	}
+	if err := logutil.InitZapLogger(cfg.Log.ToLogConfig()); err != nil {
+		return err
+	}
+	if err := kvstore.Register("unistore", mockstore.EmbedUnistoreDriver{}); err != nil {
+		return err
+	}
+	storage, err := kvstore.New(fmt.Sprintf("%s://%s", cfg.Store, cfg.Path))
+	if err != nil {
+		return err
+	}
+	dom, err := session.BootstrapSession(storage)
+	if err != nil {
+		return err
+	}
+	driver := server.NewTiDBDriver(storage)
+	svr, err := server.NewServer(cfg, driver)
+	if err != nil {
+		return err
+	}
+
+	svr.SetDomain(dom)
+	svr.InitGlobalConnID(dom.ServerID)
+	go dom.ExpensiveQueryHandle().SetSessionManager(svr).Run()
+	dom.InfoSyncer().SetSessionManager(svr)
+
+	go func() {
+		if err := svr.Run(); err != nil {
+			panic(err)
+		}
+	}()
+
+	return nil
+}
+
 func (d *mytidb) Open(dsn string) (driver.Conn, error) {
 	once.Do(func() {
-		myCfg, err := mysql.ParseDSN(dsn)
-		if err != nil {
-			onceErr = err
-			return
-		}
-		cfg := config.NewConfig()
-		cfg.Path = "/tmp/mytidb"
-		cfg.Status.ReportStatus = false
-		cfg.Log.Level = "fatal"
-		if host, sport, err := net.SplitHostPort(myCfg.Addr); err != nil {
-			onceErr = err
-			return
-		} else if iport, err := strconv.Atoi(sport); err != nil {
-			onceErr = err
-			return
-		} else {
-			cfg.Host = host
-			cfg.Port = uint(iport)
-		}
-		if onceErr = cfg.Valid(); onceErr != nil {
-			return
-		}
-		if onceErr = logutil.InitZapLogger(cfg.Log.ToLogConfig()); onceErr != nil {
-			return
-		}
-		if onceErr = kvstore.Register("unistore", mockstore.EmbedUnistoreDriver{}); onceErr != nil {
-			return
-		}
-		storage, err := kvstore.New(fmt.Sprintf("%s://%s", cfg.Store, cfg.Path))
-		if err != nil {
-			onceErr = err
-			return
-		}
-		dom, err := session.BootstrapSession(storage)
-		if err != nil {
-			onceErr = err
-			return
-		}
-		driver := server.NewTiDBDriver(storage)
-		svr, err := server.NewServer(cfg, driver)
-		if err != nil {
-			onceErr = err
-			return
-		}
-
-		svr.SetDomain(dom)
-		svr.InitGlobalConnID(dom.ServerID)
-		go dom.ExpensiveQueryHandle().SetSessionManager(svr).Run()
-		dom.InfoSyncer().SetSessionManager(svr)
-
-		go func() {
-			if err := svr.Run(); err != nil {
-				panic(err)
-			}
-		}()
+		onceErr = startTidb(dsn)
 	})
 
 	if onceErr != nil {
